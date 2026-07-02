51caececa00d3e9166b89736a9c17a88b2029ab5
sudeepa77 <sudeepareddy295@gmail.com>
2026-07-02 13:59:31 +0530

    Added main file

---
+++ Exercise-1-LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,24 @@
+package com.library;
+
+import com.library.repository.BookRepository;
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookRepository repository =
+                context.getBean("bookRepository", BookRepository.class);
+
+        BookService service =
+                context.getBean("bookService", BookService.class);
+
+        repository.displayRepository();
+        service.displayService();
+    }
+}
